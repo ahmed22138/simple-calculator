e1395542c0842018aa35c0fcd04989dccbc285e4
Ahmedmalik <159722798+ahmed22138@users.noreply.github.com>
2024-03-23 03:29:13 +0500

    simple-calculator

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env  node
+
+import inquirer from 'inquirer';
+
+const answer = await inquirer. prompt ([
+    {message:"Enter firstnum", type:"number", name:"firstnum"},
+{ message:"Enter secondnum", type:"number", name:"secondnum"},
+
+{message:"Select one of he opreator to perform opreation",
+type:"list",
+name:"operator",
+choices:["Addition","Subtraction","Multiplication","Division"],
+}
+]);
+
+if(answer.operator === "Addition") {
+    console.log(answer.firstnum + answer.secondnum);
+} else if(answer.operator === "Subtraction") {
+    console.log(answer.firstnum - answer.secondnum);
+} else if(answer.operator === "Multiplication") {
+    console.log(answer.firstnum * answer.secondnum);
+} else if(answer.operator === "Division") {
+    console.log(answer.firstnum / answer.secondnum);
+} else {
+    console.log("Enter Valid operator");
+}
